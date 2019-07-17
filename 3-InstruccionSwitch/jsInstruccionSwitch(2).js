beeb853039ebab13a63ae5fd5097e80067ad7d85
vict182 <52429707+vict182@users.noreply.github.com>
2019-07-16 22:06:36 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,9 +3,29 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
 
 
+switch (mesDelAño) { 
+    
+        case "Julio" :
+        case  "Agosto" : alert ("Abrigate que hace frio!")  
+              
+        break 
 
+        case "Enero" :
+        case "Febrero" :
+        case "Marzo" :
+        case "Abril" :
+        case "Mayo" :
+        case "Junio" : alert ("Falta para invierno")
 
-}//FIN DE LA FUNCIÓN
+        break 
+
+        case "Septiembre" :
+        case "Octubre" :
+        case "Noviembre" :
+        case "Diciembre " : alert("Ya pasamos el frio ahora calor!!")
+
+}
+
+}
